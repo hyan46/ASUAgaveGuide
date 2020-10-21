0afc17f80176c1b6a6558500285d6eea9e526931
Hao Yan <yanhaopku@gmail.com>
2020-10-20 20:42:35 -0700

    Update run.sh

--- test/matlab/run.sh
+++ test/matlab/run.sh
@@ -4,6 +4,8 @@
 #SBATCH --error=job.%J.err
 #SBATCH --output=job.%J.out
 #SBATCH --array 1-3
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=$USER@asu.edu 
 
 module load matlab/2016a
 matlab -nodisplay -r "run, quit"
